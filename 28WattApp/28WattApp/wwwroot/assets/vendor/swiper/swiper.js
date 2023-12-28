f88cf339fc61841a867abadf77c6b7cc3714b9fe
Dipali <dipali.facile@outlook.com>
2023-12-27 18:51:19 -0800

    slider, faq and requestquote components in home page

---
+++ 28WattApp/28WattApp/wwwroot/assets/vendor/swiper/swiper.js
@@ -0,0 +1,36 @@
+﻿new Swiper('.customer-slider', {
+    speed: 400,
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        }
+    }
+});
